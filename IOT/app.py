edb5207be9b44225e96026188a5adf207c96391e
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2018-11-24 01:49:03 +0600

    app.py refactored

--- IOT/app.py
+++ IOT/app.py
@@ -1,4 +1,3 @@
-
 import math
 import socket
 import sys
@@ -37,16 +36,7 @@ GPIO.setmode(GPIO.BOARD)
 
 bin_depth = 100
 garbage = {}
-FINISH = True
-FINISH_D = False
-
-# db configuration
-# config = {
-#     "apiKey": "AIzaSyDOEg7tnAMj6Y0z2BCO8wvnexZxOrUcVZw",
-#     "authDomain": "gmsusingpi.firebaseapp.com",
-#     "databaseURL": "https://gmsusingpi.firebaseio.com/",
-#     "storageBucket": "gmsusingpi.appspot.com"
-# }
+isRunning = True
 
 
 def garbage_calc(distance, trash_height):
@@ -55,39 +45,6 @@ def garbage_calc(distance, trash_height):
     garbage_percent = (1 - math.floor(distance) / trash_height) * 100
     print(garbage_percent)
     return round(garbage_percent)
-
-
-# def garbage_monitor(sensor_id, garbage, notification, threshold):
-#     if garbage['current'] <= 0:
-#         lcd.show("{}:".format(sensor_id), "empty")
-#         notification.setIsSent(False)
-
-#     elif garbage['current'] > 0 and garbage['current'] <= 60 :
-#         lcd.show("{}:".format(sensor_id), "{}%".format(garbage['current']))
-
-#     elif garbage['current'] > 60 and garbage['current'] <=70:
-#         lcd.show("{}:".format(sensor_id),"almost full")
-
-#     else:
-#         lcd.show("{}:".format(sensor_id), "full!")
-#         if garbage['current']>=threshold and notification.isSent()!=True:
-#             lcd.show("{}:".format(sensor_id), "sms sending..")
-#             notification.notifyMe(sensor_id)
-#             notification.setIsSent(True)
-#             time.sleep(1)
-
-# check internet connection
-
-isRunning = True
-
-def is_connected():
-    try:
-        socket.create_connection(("www.google.com", 80))
-        return True
-    except OSError:
-        pass
-    return False
-
 
 def database_change():
     print("Database Thread Started")
@@ -105,29 +62,6 @@ def database_change():
 def change_running_state(state):
     global isRunning
     isRunning = state
-
-class GarbageBin(threading.Thread):
-    def __init__(self, sensor_name, distance, trigger, echo, gpio):
-        threading.Thread.__init__(self)
-        self.gpio = gpio
-        self.gpio.setmode(gpio.BOARD)
-        self.trash = Sensor(sensor_name, distance, trigger, echo)
-        self.location = Geocode(requests)
-        self.db = Database(conn, datetime)
-        self.notification = Notification(Client, False)
-        self.trash.initSensor(self.gpio)
-        self._stop_event = threading.Event()
-
-    def run(self):
-        while True:
-            run_sensor(self.trash, self.location, self.db, self.notification)
-
-    def stop(self):
-        self._stop_event.set()
-
-    def stopped(self):
-        return self._stop_event.is_set()
-
 
 def run_sensor(trash):
     global isRunning
@@ -158,57 +92,15 @@ def run_sensor(trash):
 
 def main():
     global isRunning
-    trash = Sensor(sys.argv[1], 0, int(sys.argv[2]), int(sys.argv[3]))
-    d_thread = threading.Thread(name='d_thread', target=database_change)
-    # sensor_thread = GarbageBin("TRASH_01", 0, 5, 7, GPIO)
-    d_thread.start()
 
-    run_sensor(trash)
-    # time.sleep(2)
-    # sensor_thread.start()
-    # bins = []
-    # menu = "1. Add New Bin\t2. Delete a Bin\n3. Exit"
-
-    # Get bin_docs from Database
-    # bin_docs = r.table('bin').run(conn)
-
-    # for bin in bin_docs:
-    #     bins.append(GarbageBin(bin['name'], bin['distance'], bin['trig_pin'], bin['echo_pin']))
-    #     # print(bin['name'])
-
-    # time.sleep(2)
-
-    # for bin in bins:
-    #     bin.start()
-    # while True:
-    #     print(menu)
-    #     option = int(input("Please Enter Your Option >> "))
-    #     print(option)
-    #     if option == 3:
-            # FINISH = False
-            # FINISH_D = True
-            # print("Joining")
-            # d_thread.join()
-            # print("Database Thread Stopped")
-            # sensor_thread.stop()
-            # sensor_thread.join()
-            # print("Sensor Thread Stopped")
-            # sys.exit()
-    # trash
-    # while True:
-    #     if trash.TUNED == True:
-    #         trash.measureDistance(GPIO, time)
-    #         time.sleep(2)
-    #         garbage['current'] = garbage_calc(math.floor(trash.getDistance()), bin_depth) #trash_height=25
-    #         db.updateDatabase(trash.getSensorId(), garbage, location)
-    #         garbage_monitor(trash.getSensorId(),garbage, notification,threshold=80)
-    #     else:
-    #         bin_depth = math.floor(trash.tuneSensor(GPIO,time))
-    #         garbage['depth'] = bin_depth
-    #         print("Bin Depth %d"%bin_depth)
-       
-
-
+    sensor_id = sys.argv[1]
+    bin = r.table('bin').get(sensor_id).run(conn)
+    if bin is not None:
+        trash = Sensor(bin['name'], 0, int(bin['trig_pin']), int(bin['echo_pin']))
+        d_thread = threading.Thread(name='d_thread', target=database_change)
+        d_thread.start()
+        run_sensor(trash)
+    
 if __name__ == '__main__':
     try:
         main()
